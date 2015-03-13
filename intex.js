a12bddd3fbcd6a2e3602ef7aa1365c0d92d131ac
dbat <aadbox-5@yahoo.com>
2015-03-13 20:55:18 +0700

    doc:-

--- intex.js
+++ intex.js
@@ -10,7 +10,7 @@ All right reserved
   at first, we would like to make them object-oriented sounds,
   with fancy range checking (means several method calls) almost
   in every turns, but finally we decided to cut te red tapes
-  down to the real bone crunching bytes, left only the sane
+  down to the real bone crunching buffer, left only the sane
   consumer happy, the careless ones may go to the nexus.
 
 */
